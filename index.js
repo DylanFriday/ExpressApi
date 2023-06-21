d522585e0d5b6b7a289c03c5902fdc6106d46ea0
sitt hmue eain <yoopajdoto@gmail.com>
2023-06-21 11:07:31 +0630

    get/post

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send({ message: 'Hello from the server side', app: 'Natours' });
-});  
+app.use(express.json());
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+// getRequest
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+});
+// postRequest
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'success', data: { tour: newTour } });
+    }
+  );
+});
 
-app.post('/',(req,res)=>{
-    res
-        .status(200)
-        .send('You can post to this URL')
-})
- 
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}....`);
